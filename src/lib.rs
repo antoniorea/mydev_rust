6b1f00044057b62a8593781065f7be45a58d7ce2
antonio <antonio@system-12.mydomain.net>
2025-07-28 15:52:24 +0200

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(unused)]
+use std::io;
+
+
+
+pub fn string_to_float ( ) -> f64 {
+
+    println!("Introduzca un numero :");
+
+        let mut n1 = String::new();
+
+        io::stdin().read_line(&mut n1).expect("cant read line");
+
+        n1.trim().parse::<f64>().expect("no number")
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use mydev_rust;
+
+fn main() {
+
+    let  a = mydev_rust::string_to_float ( );
+    let  b = mydev_rust::string_to_float ( );
+
+
+        println!("La suma de {:2} y {:2} es : {:2}", a , b, suma(a, b));
+
+}
+
+fn suma(n1 : f64, n2 : f64) -> f64 {
+    n1 + n2
+}
